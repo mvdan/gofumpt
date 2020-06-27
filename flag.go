86fd224e41946b5564a22d2331aa5794260f5254
Daniel Martí <mvdan@mvdan.cc>
2020-06-27 14:40:47 +0100

    make gofumpt's -s flag default to true

--- flag.go
+++ flag.go
@@ -6,3 +6,8 @@ package main
 import "flag"
 
 var langVersion = flag.String("lang", "", "target Go version in the form 1.X (default from go.mod)")
+
+func init() {
+	// make -s default to true
+	*simplifyAST = true
+}
