7ed9b835dfb3ef6ff25583e5423be626233be73f
Daniel Martí <mvdan@mvdan.cc>
2021-01-02 13:47:22 +0000

    gofumports: re-generate with tools dep update

--- gofumports/internal/event/core/event.go
+++ gofumports/internal/event/core/event.go
@@ -12,7 +12,7 @@ import (
 	"mvdan.cc/gofumpt/gofumports/internal/event/label"
 )
 
-// Event holds the information about an event of note that ocurred.
+// Event holds the information about an event of note that occurred.
 type Event struct {
 	at time.Time
 
